6fd25802b883b8a981080134424dcaea0549b251
RammSanker <109334253+rammsanker54@users.noreply.github.com>
2022-11-11 16:50:34 +0530

    word with "a"

---
+++ a containing words/main,.js
@@ -0,0 +1,12 @@
+var a =["arun","elonmusk","sam","Ramm","praveena"]
+for(let i=0;i<a.length;i++){
+  let  temp=a[i]
+//   console.log(a[i])
+    for(let b=0;b<temp.length;b++){
+        if(temp[b]==="a") {
+            
+            console.log(temp)
+        }
+    }
+    
+}
